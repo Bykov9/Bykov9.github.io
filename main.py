ec51ed41c1a8d62f530648e8b548b5a2ed9478ce
Bykov9 <egoregor77999@gmail.com>
2025-03-05 22:56:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ app = Flask(__name__) #creating flask app name
 
 @app.route('/')
 def home():
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
 @app.route('/resume_1')
 def resume_1():
-    return render_template("templates/resume_1.html")
+    return render_template("resume_1.html")
 
 @app.route('/resume_template')
 def resume_template():
-    return render_template("templates/resume_template.html")
+    return render_template("resume_template.html")
 
 if(__name__ == "__main__"):
     app.run()
